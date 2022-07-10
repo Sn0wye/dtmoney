09be06aee82456e2ee8645e5f9d2711b9e5c252c
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-09 23:03:29 -0300

    format and show summary info

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,6 +9,25 @@ import { useTransactions } from '../../hooks/useTransactions';
 export const Summary = () => {
   const { transactions } = useTransactions();
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'deposit') {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
+
   return (
     <Container>
       <div>
@@ -16,21 +35,36 @@ export const Summary = () => {
           <p>Incomes</p>
           <img src={incomeImg} alt='Income icon' />
         </header>
-        <strong>R$1000</strong>
+        <strong>
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Outcomes</p>
           <img src={outcomeImg} alt='Outcome icon' />
         </header>
-        <strong>- R$500</strong>
+        <strong>
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          }).format(summary.withdraws)}
+        </strong>
       </div>
       <div className='highlight-background'>
         <header>
           <p>Total</p>
           <img src={totalImg} alt='Total icon' />
         </header>
-        <strong>R$500</strong>
+        <strong>
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
