200b4c01d9758636d0bb2aa19c19053cac27b4c9
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-31 18:06:38 -0300

    feat: :sparkles: add switch functions and login modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { LoginModal } from './components/LoginModal';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { RegisterModal } from './components/RegisterModal';
 import { AuthProvider } from './hooks/useAuth';
@@ -13,14 +14,29 @@ Modal.setAppElement('#root');
 export const App = () => {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
     useState(false);
-  const [isRegisterModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
   function handleToggleNewTransactionModal() {
     setIsNewTransactionModalOpen(!isNewTransactionModalOpen);
   }
 
   function handleToggleRegisterModal() {
-    setIsLoginModalOpen(!isRegisterModalOpen);
+    setIsRegisterModalOpen(!isRegisterModalOpen);
+  }
+
+  function handleToggleLoginModal() {
+    setIsLoginModalOpen(!isLoginModalOpen);
+  }
+
+  function switchBetweenModals() {
+    if (isRegisterModalOpen) {
+      setIsRegisterModalOpen(false);
+      setIsLoginModalOpen(true);
+    } else {
+      setIsLoginModalOpen(false);
+      setIsRegisterModalOpen(true);
+    }
   }
 
   return (
@@ -41,6 +57,13 @@ export const App = () => {
         <RegisterModal
           isOpen={isRegisterModalOpen}
           onRequestClose={handleToggleRegisterModal}
+          onSwitchModalRequest={switchBetweenModals}
+        />
+
+        <LoginModal
+          isOpen={isLoginModalOpen}
+          onRequestClose={handleToggleLoginModal}
+          onSwitchModalRequest={switchBetweenModals}
         />
       </TransactionsProvider>
     </AuthProvider>
